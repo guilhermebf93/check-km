f4549c220d3958e93eca521bb1846597079dd972
Guilherme Ferreira <guilherme.bf93013@gmail.com>
2026-06-29 23:09:44 -0300

    Ajuste no layout da login page e alteração no action-card para aceitar callback

--- src/app/components/ui/ActionCard/index.tsx
+++ src/app/components/ui/ActionCard/index.tsx
@@ -1,24 +1,54 @@
+'use client'
 import styles from './styles.module.scss'
 
 import Link from 'next/link'
 
 import type { LucideIcon } from 'lucide-react'
 
-export type ActionCardProps = {
+type BaseActionCardProps = {
   icon: LucideIcon,
   label: string,
-  href: string
 }
+
+type LinkActionCardProps = BaseActionCardProps & {
+  href: string,
+  onClick?: never,
+}
+
+type CallBackActionCardProps = BaseActionCardProps & {
+  href?: never,
+  onClick: () => void,
+}
+
+export type ActionCardProps = 
+  | LinkActionCardProps
+  | CallBackActionCardProps
 
 export function ActionCard({
   icon: Icon,
   label,
-  href
+  href,
+  onClick,
 }: ActionCardProps) {
 
-  return(
-    <Link
-      href={href}
+  if (href) {
+    return(
+      <Link
+        href={href}
+        className={styles.cardContainer}
+      >
+        <Icon
+          className={styles.cardIcon}
+          aria-hidden
+        />
+        <span>{label}</span>
+      </Link>
+    )
+  }
+
+  return (
+    <div
+      onClick={onClick}
       className={styles.cardContainer}
     >
       <Icon
@@ -26,6 +56,6 @@ export function ActionCard({
         aria-hidden
       />
       <span>{label}</span>
-    </Link>
+    </div>
   )
 }
